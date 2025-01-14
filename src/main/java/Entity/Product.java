7bd3acc7c780ad435a4bf9c61b9654ec9302a572
Rakesh Gupta <rakeshgupta1361411@gmail.com>
2025-01-14 21:09:47 +0530

    main

---
+++ src/main/java/Entity/Product.java
@@ -0,0 +1,15 @@
+package Entity;
+
+import lombok.Data;
+
+@Data
+public class Product {
+	
+	private int id;
+	private String name;
+	private double price;
+	private String description;
+	
+	
+	
+}
